4faa62a707827e3fa961589efe738a5741488b53
aysukorkmaz <korkmazaysu12@gmail.com>
2022-12-06 14:59:27 +0000

    Using task 4 combined strings

--- 4.sh
+++ 4.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-echo "Please enter your first name" 
+echo "Please enter your first name"     #Taking information from user 
 read first_name
 
 echo "Please enter your second name"
-read second_name
+read second_name			#User input 
 
 echo "Please enter your email"
 read email
@@ -13,8 +13,6 @@ read email
 echo "Please enter your phone number"
 read number
 
-FULL_NAME="$first_name $second_name"
-echo "Welcome to Furnitas $FULL_NAME";
+FULL_NAME="$first_name $second_name"    #String combined, Variable = FULL_NAME and is made up of first and second name
+echo "Welcome to Furnitas $FULL_NAME";  #User's full name will be included in the message 
 echo "Thank you for signing up with us"
-
-
