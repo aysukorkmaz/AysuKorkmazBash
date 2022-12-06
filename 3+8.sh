7cdcb870e7ad2126e54a07453d622228baa172d2
aysukorkmaz <korkmazaysu12@gmail.com>
2022-12-06 14:49:56 +0000

    Create file shoiwcasing use of task 3 and 8 - to install packages

--- 3+8.sh
+++ 3+8.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Which software would you like to download?"
+echo "Which software would you like to download?" #Asks admin what package they'd like to install 
 
-echo "1 - Apache"
+echo "1 - Apache"      #Deliver four options 
 echo "2 - Nginx"
 echo "3 - MariaDB"
 echo "4 - phpMyAdmin"
 
-read choice;
+read choice;       #User picks whichever one they'd like to install
 
-case $choice in
+case $choice in    #Depending on which number user chooses, automatic installation of that software occurs. 
 	1) 
 	sudo apt update
    	sudo apt install apache2
